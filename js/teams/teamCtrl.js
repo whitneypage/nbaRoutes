e4271b36504418d333125e1c9b5c6770118cb70f
whitneypage <wpagegarner@gmail.com>
2015-04-20 17:31:50 -0600

    Not Done

--- js/teams/teamCtrl.js
+++ js/teams/teamCtrl.js
@@ -1,5 +1,50 @@
 var app = angular.module('nbaRoutes');
 
-app.controller('teamCtrl', function($scope, $routeParams, teamService){
+app.controller('teamCtrl', function($scope, $routeParams, teamService, teamData){
+
+$scope.teamData = teamData;
+$scope.newGame  = {};
+$scope.showNewGameForm = false;
+$scope.toggleNewGameForm = function() {
+	if ($scope.showNewGameForm) {
+		$scope.showNewGameForm = false;
+	} else {
+		$scope.showNewGameForm = true;
+	}
+}
+
+
+if ($routeParams.team === 'utahjazz') {
+	$scope.homeTeam = "Utah Jazz";
+	$scope.logoPath = "images/jazz-logo.png";
+}
+if ($routeParams.team === 'miamiheat') {
+	$scope.homeTeam = "Miami Heat";
+	$scope.logoPath = "images/heat-logo.png"
+}
+if ($routeParams.team === 'losangeleslakers') {
+	$scope.homeTeam = "Los Angeles Lakers";
+	$scope.logoPath = "images/lakers-logo.png"
+}
+
+$scope.submitGame = function() {
+	$scope.newGame = {
+		homeTeam: $scope.homeTeam.split(' ').join('').toLowerCase()
+	}
+	teamService.addNewGame($scope.newGame)
+	.then(function() {
+		teamService.getTeamData($scope.newGame.homeTeam)
+		.then(function(info) {
+			return info;
+		})
+		
+	})
+
+$scope.newGame = {};
+$scope.showNewGameForm = false;
+}
+console.log($scope.newGame);
+
+
 
 });
--- js/teams/teamService.js
+++ js/teams/teamService.js
@@ -2,4 +2,46 @@ var app = angular.module('nbaRoutes');
 
 app.service('teamService', function($http, $q){
 
+   this.addNewGame = function(gameObj) {
+   	var url = "https://api.parse.com/1/classes/" + gameObj.homeTeam;
+   	if (gameObj.homeTeamScore.parseInt() > gameObj.opponentScore.parseInt()) {
+   		gameObj.won = true;
+   	} else {
+   		gameObj.won = false;
+   	}
+   	$http({
+   		method: 'POST',
+   		url: url,
+   		data: gameObj
+   	})
+   }
+   
+  
+   var dfd = $q.defer();
+   this.getTeamData = function(team) {
+      var url = 'https://api.parse.com/1/classes/' + team;
+      $http ({
+      	method: 'GET',
+      	url: url
+      }).then(function(data){
+      	var results = data.data.results;
+      	console.log(results);
+      	var wins = 0;
+      	var losses = 0;
+      	for (var i = 0; i < results.length; i++) {
+      		if (results[i].won) {
+      			wins++;
+      		} else if (!results[i].won) {
+      			losses++;
+      		}
+      	}
+      	results.wins = wins;
+      	results.losses = losses;
+      	dfd.resolve(data);
+      })
+       return dfd.promise;
+   }
+
+
+
 });
